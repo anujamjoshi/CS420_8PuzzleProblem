e063e0deeeae314ffaa216571bfa557a62a938cb
amjoshi <amjoshi@cpp.edu>
2017-04-30 20:09:11 -0700

    firstCommit

---
+++ src/Board.java
@@ -0,0 +1,288 @@
+import java.util.ArrayList;
+
+/**
+ * @class Board.java 
+ * This class is the class that contains the state of the current state as an object Board. 
+ * @author Anuja Joshi
+ *
+ */
+
+public class Board {
+	private int [][] board = new int[3][3];
+	private int heuristic; 
+	private boolean h1; 
+	private Board parent; 
+	private ArrayList<Board> children = new ArrayList<Board>(); 
+	private Board nextState; 
+	private int zeroRow; 
+	private int zeroCol;
+	private int gValue; 
+
+
+	/**
+	 * @return the board
+	 */
+	public int[][] getBoard() {
+		return board;
+	}
+	/**
+	 * @return zeroRow
+	 */
+	public int getZeroRow() {
+		return zeroRow;
+	}
+	/**
+	 * @return zeroCol
+	 */
+	public int getZeroCol() {
+		return zeroCol;
+	}
+
+	/**
+	 * @return the heuristic
+	 */
+	public int getHeuristic() {
+		return heuristic;
+	}
+	/**
+	 * @return the parent
+	 */
+	public Board getParent() {
+		return parent;
+	}
+	/**
+	 * @return the children
+	 */
+	public ArrayList<Board> getChildren() {
+		return children;
+	}
+	/**
+	 * @return the nextState that is supposed to be expanded 
+	 */
+	public Board getNextState() {
+		return nextState;
+	}
+	/**
+	 * takes in @param values as a one dimensional array 
+	 * and populates the current Board instance with the values passed in
+	 * also takes in a @param h1 as a boolean value to indicate if h1 or h2 is being used for the heruistic 
+	 */
+	public Board(int[]values, boolean h, Board p, int g){
+		int i =0; 
+		for(int row=0; row <3; row++){
+			for (int col=0; col<3; col++){
+				board[row][col]= values[i];
+				if (values[i]==0){
+					zeroRow=row; 
+					zeroCol=(col); 
+				}
+				i++;
+			}
+		}
+		if (h ==true){
+			heuristic = this.h1();
+
+		}
+		else {
+			heuristic = this.h2();
+		}
+		parent = p; 
+		nextState=null; 
+		gValue=g; 
+		h1 = h;
+	}
+	public Board(int[][]values, boolean h, Board p, int g){
+
+		for(int row=0; row <3; row++){
+			for (int col=0; col<3; col++){
+				board[row][col]= values[row][col];
+				if (values[row][col]==0){
+					zeroRow=row; 
+					zeroCol=(col); 
+				}
+			}
+		}
+		if (h ==true){
+			heuristic = this.h1();
+		}
+		else {
+			heuristic = this.h2();
+		}
+		parent = p; 
+		nextState=null; 
+		gValue=g; 
+		h1 = h;
+	}
+	/**
+	 * @return boolean value that determines if the board is complete
+	 */
+	public boolean isFinal(){
+
+		for(int row=0; row <3; row++){
+			for (int col=0; col<3; col++){
+
+				if (row*3 +col != board[row][col]){
+					return false; 
+				}
+			}
+
+		}
+		return true;
+	}
+	/**
+	 * @returns boolean if the board is solvable 
+	 */
+	public boolean isSolveable(){
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		for (int i = 0; i < 3; i++) {
+			// tiny change 1: proper dimensions
+			for (int j = 0; j < 3; j++) { 
+				// tiny change 2: actually store the values
+				if (board[i][j]>0){
+					list.add(board[i][j]); 
+				}
+			}
+		}
+		int numInversion = inversion(list);
+		System.out.println(numInversion);
+		return (numInversion%2) == 0; 
+	}
+	private int inversion(ArrayList<Integer> list) {
+		int count =0; 
+		for (int i = 0; i < 7; i++){
+			for (int j = i+1; j < 8; j++){
+				if (list.get(i)<list.get(j)){
+					count++;
+				}
+			}
+		}
+		return count;
+	}
+	/**
+	 * @returns string value that is the string version of the current board
+	 */
+	public void printBoard(){
+		String output = "";
+		for(int row=0; row <3; row++){
+			for (int col=0; col<3; col++){
+				output+=board[row][col] +"\t";
+			}
+			output+="\n";
+		}
+		System.out.println( output);
+	}
+
+
+
+	/**
+	 * @returns the number of misplaced tiles, the heuristic h1 
+	 * Disregards the 0 value as it is the representation for the empty tile
+	 */
+	private int h1(){
+
+		int count =0; 
+		for(int row=0; row <3; row++){
+			for (int col=0; col<3; col++){
+
+				if (row*3 +col != board[row][col] && board[row][col]!=0){
+					count++; 
+				}
+			}
+
+		}
+		return count;
+
+	}
+	/**
+	 * @returns the sum of the distances of the tiles from their goal positions, the heuristic h2
+	 * uses the formula of row*3 + column = value that's supposed to be there. 
+	 * Disregards the 0 value as it is the representation for the empty tile
+	 */
+	private int h2(){
+		int count =0; 
+		for (int row = 0; row <3; row++){
+			for (int col =0; col <3; col++){
+				if (row*3 +col != board[row][col] && board[row][col]!=0){
+					count+= calculateMoves(row, col,board[row][col] );
+				}
+			}
+		}
+		return count;
+	}
+	/**
+	 * @param row
+	 * @param col
+	 * @param value is the value that we are trying to calculate the distance for 
+	 * @returns an integer value that is the number of moves that are necessary for the move 
+	 * used in {@code #h2()}
+	 */
+	private int calculateMoves(int row, int col, int value) {
+		int colFinal = value %3; 
+		int rowFinal = (value-colFinal)/3;
+		return  Math.abs(colFinal-col+rowFinal-row);
+	}
+	/**
+	 * uses the zeroRow and zeroCol to get the potential boards that can be expanded 
+	 * after which uses the lowestCost method to find the next step
+	 */
+	public void addChildren(){
+		//get the left child board 
+		if (zeroRow -1>=0){
+			//int[][]values, boolean h1, Board p, int g
+			children.add(new Board(swap(zeroRow,zeroCol,zeroRow-1,zeroCol), this.h1, this, gValue+1));
+		}
+		if (zeroRow +1<=2){
+			//int[][]values, boolean h1, Board p, int g
+			children.add(new Board(swap(zeroRow,zeroCol,zeroRow+1,zeroCol), this.h1, this, gValue+1));
+		}
+		if (zeroCol -1>=0){
+			//int[][]values, boolean h1, Board p, int g
+			children.add(new Board(swap(zeroRow,zeroCol,zeroRow,zeroCol-1), this.h1, this, gValue+1));
+		}
+		if (zeroCol +1<=2){
+			//int[][]values, boolean h1, Board p, int g
+			children.add(new Board(swap(zeroRow,zeroCol,zeroRow,zeroCol+1), this.h1, this, gValue+1));
+		}
+		this.nextState=this.getLowestCost();
+	}
+	private int[][] swap (int r1, int c1, int r2, int c2){
+		int [][]tempBoard = new int[3][3];
+		for(int i =0; i<3; i++){
+			for (int j = 0; j <3; j++){
+				tempBoard[i][j] = board[i][j];
+			}
+		}
+		int temp = board[r1][c1];
+		tempBoard[r1][c1] = board[r2][c2];
+		tempBoard[r2][c2] = temp;
+		//		String output = "";
+		//		for(int row=0; row <3; row++){
+		//			for (int col=0; col<3; col++){
+		//				output+=tempBoard[row][col] +"\t";
+		//			}
+		//			output+="\n";
+		//		}
+		//	System.out.println("HI\n" + output);
+		return tempBoard; 
+
+	}
+	private int calculateCost(){
+		return gValue+this.getHeuristic(); 
+	}
+	private Board getLowestCost(){
+		if (children.size()==0){
+			return null; 
+		}
+		int minCost = children.get(0).calculateCost(); 
+		Board min = children.get(0);
+		for(Board b:children){
+			if (b.calculateCost()<minCost){
+				minCost = b.calculateCost(); 
+				min=b;
+			}
+		}
+		return min;
+	}
+
+
+}
---
+++ src/Driver.java
@@ -0,0 +1,163 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Scanner;
+
+
+public class Driver {
+
+	/**
+	 * @param args
+	 */
+	static ArrayList<Board> path = new ArrayList<Board>(); 
+	static int depth =0 ;
+	static HashMap<Integer, Long> averageTimeh1 = new HashMap<Integer,Long>();
+	static ArrayList <Long> timeh1 = new ArrayList<Long>(); 
+	static HashMap<Integer, Long> averageTimeh2 = new HashMap<Integer,Long>();
+	static ArrayList <Long> timeh2 = new ArrayList<Long>(); 
+
+	public static int[] randomInput(){
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		list.add(0);
+		list.add(1);
+		list.add(2);
+		list.add(3);
+		list.add(4);
+		list.add(5);
+		list.add(6);
+		list.add(7);
+		list.add(8);
+		Collections.shuffle(list);
+		System.out.println(list);
+		int [] a = new int[9];
+		for(int i =0 ; i < 9; i++){
+			a[i]=list.get(i);
+		}
+		return a;
+	}
+	public static int[] userInput(){
+		int[] a = new int[9];
+		Scanner sc = new Scanner (System.in);
+		System.out.println("enter in a specific 8-puzzle configuration entered through the standard input, which contains the configuration for only one puzzle");
+		System.out.println("Where 0 represents the empty tile.");
+		System.out.println("please enter the top row in the format of:\na b c\t where a b c are the numbers");
+		String [] input = sc.nextLine().split(" ");
+		while (input.length!=3){
+			System.out.println("Incorrect input! please enter the top row in the format of:\na b c\t where a b c are the numbers");
+			input = sc.nextLine().split(" ");
+
+		}
+		int index =0; 
+		while(index <3){
+			a[index] = Integer.valueOf(input[index]);
+			index ++; 
+		}
+		System.out.println("please enter the second row in the format of:\nd e f\t where a b c are the numbers");
+		input = sc.nextLine().split(" ");
+		while (input.length!=3){
+			System.out.println("Incorrect input! please enter the second row in the format of:\nd e f\t where a b c are the numbers");
+			input = sc.nextLine().split(" ");
+
+		}
+		index =0; 
+		while(index <3){
+			a[index + 3] = Integer.valueOf(input[index]);
+			index ++; 
+		}
+		System.out.println("please enter the third row in the format of:\ng h i\t where a b c are the numbers");
+		input = sc.nextLine().split(" ");
+		while (input.length!=3){
+			System.out.println("Incorrect input! please enter the third row in the format of:\ng h i\t where a b c are the numbers");
+			input = sc.nextLine().split(" ");
+
+		}
+		index =0; 
+		while(index <3){
+			a[index + 6] = Integer.valueOf(input[index]);
+			index ++; 
+		}
+		sc.close();
+		return a;
+	}
+	public static void fileInput() throws FileNotFoundException{
+
+		File c = new File("input.txt");
+		Scanner scan = new Scanner(c);
+		while(scan.hasNext()){
+			String[] s = scan.nextLine().split(" ");
+
+			if (s[0].equals("Depth")){
+				if (depth!=0){
+
+					Long totalTime =(long) 0; 
+					for(long l: timeh1){
+						totalTime+=l;
+					}
+
+					System.out.println("Total time " + totalTime );
+					averageTimeh1.put(depth, totalTime/timeh1.size());
+					 totalTime =(long) 0; 
+					for(long l: timeh2){
+						totalTime+=l;
+					}
+
+					System.out.println("Total time " + totalTime );
+					averageTimeh2.put(depth, totalTime/timeh2.size());
+				}
+				depth = Integer.parseInt(s[1]);
+
+
+			}
+			else {
+				long startTime =  System.currentTimeMillis();
+				int [] a = new int[9];
+				for(int index = 0; index < 9; index++){
+					a[index] = Integer.valueOf(s[0].charAt(index)+"");
+
+
+				}
+				for (int i : a){
+					System.out.print(i );
+				}
+				System.out.println();
+				runh1(a);
+				timeh1.add(System.currentTimeMillis()-startTime);
+				startTime =System.currentTimeMillis();
+				runh2(a);
+				timeh2.add(System.currentTimeMillis()-startTime);
+				
+			}
+		}
+		if (depth!=0){
+
+			Long totalTime =(long) 0; 
+			for(long l: timeh1){
+				totalTime+=l;
+			}
+			averageTimeh1.put(depth, totalTime/timeh1.size());
+			 totalTime =(long) 0; 
+			for(long l: timeh2){
+				totalTime+=l;
+			}
+			averageTimeh2.put(depth, totalTime/timeh2.size());
+		}
+		scan.close();
+	}
+	public static void runh1(int[] input){
+		Board b = new Board(input, true, null, 0);
+		while(!b.isFinal() && b.isSolveable()){
+			
+		}
+	}
+	public static void runh2(int[] input){
+		Board b = new Board(input, false, null, 0);
+	}
+	public static void main(String[] args) throws FileNotFoundException {
+
+		fileInput(); 
+		System.out.println("HI " + averageTimeh1.get(2) + averageTimeh1.get(4));
+	}
+
+}
